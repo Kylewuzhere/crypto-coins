4f18bdf904cb388177a604ad1e1a5a65df034d14
Kylewuzhere1000 <kylevasquez@developersinstitute.co.nz>
2022-12-03 15:31:57 +1300

    style: table header

--- src/components/TableHeadButtons.js
+++ src/components/TableHeadButtons.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const TableHeadButtons = ({ label, text, handleSortChange }) => {
   return (
-    <button type="button" onClick={() => handleSortChange(label)}>
+    <button
+      className="btn btn-light"
+      type="button"
+      onClick={() => handleSortChange(label)}
+    >
       {text}
     </button>
   );
